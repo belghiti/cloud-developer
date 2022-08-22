948668b148a1698fa66899c4b34c14a9bebe9cef
idriss belghiti <belghyty@gmail.com>
2022-08-22 12:15:34 +0100

    Udagram Image Filtering Microservice

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -29,6 +29,17 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url } = req.query;
+    if(!image_url) return res.status(400).send(`required parameter 'image_url' missing !`);
+    
+    const filteredImage = await filterImageFromURL(image_url);  
+
+    return res.sendFile(filteredImage, (err) => {
+      if(!err) deleteLocalFiles([filteredImage]);
+    });
+  });
+  
   //! END @TODO1
   
   // Root Endpoint
